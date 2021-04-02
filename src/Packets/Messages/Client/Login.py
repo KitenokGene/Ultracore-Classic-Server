1283577597148c0d848516ae15e7bc6cb4ee51fa
XeonDev <49839864+XeonDevv@users.noreply.github.com>
2021-04-02 22:47:38 +0300

    Hi

---
+++ src/Packets/Messages/Client/Login.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+
+from Utils.Reader import ByteStream
+from Packets.Messages.Server.LoginOk import LoginOk
+from Packets.Messages.Server.OwnHomeData import OwnHomeData
+from Logic.Player import Player
+
+
+class Login(ByteStream):
+
+    def __init__(self, data, device):
+        super().__init__(data)
+        self.device = device
+        self.player = Player(device)
+
+    def decode(self):
+        self.player.HighID = self.ReadUint32()
+        self.player.LowID = self.ReadUint32()
+        self.player.Token = self.ReadString()
+
+        self.ReadVint()
+        self.ReadVint()
+        self.ReadVint()
+        self.ReadString()
+        self.ReadUint32()
+
+        self.device.AndroidID = self.ReadString()
+
+        self.ReadString()
+
+        self.device.DeviceModel = self.ReadString()
+        self.device.OpenUDID = self.ReadString()
+        self.device.OSVersion = self.ReadString()
+        self.device.IsAndroid = self.ReadBool()
+
+        self.ReadUint32()
+        self.ReadString()
+
+        self.device.Language = self.ReadString()
+
+    def process(self):
+        LoginOk(self.device).Send()
+        OwnHomeData(self.device).Send()
+        # Todo : Process stuff (implement OHD)
